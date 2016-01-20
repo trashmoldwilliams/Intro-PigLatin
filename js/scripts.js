d75337ac0acfa2ed8baec8e122b58a680b2db4e0
Jackson Cafazzo and Will Johnson <doctorjuno@gmail.com>
2016-01-20 11:58:27 -0800

    add and hasVowelAt and pigLatin functions and specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-var vowelCheck = function(word) {
+var pigLatin = function(englishWord) {
+  if (hasVowelAt(englishWord, 0)) {
+    var translatedWord = englishWord;
+  } else if (hasVowelAt(englishWord, 3)) {
+    var translatedWord = englishWord.slice(3) + englishWord.slice(0, 3);
+  } else if (hasVowelAt(englishWord, 2)) {
+    var translatedWord = englishWord.slice(2) + englishWord.slice(0, 2);
+  } else if (hasVowelAt(englishWord, 1)) {
+    var translatedWord = englishWord.slice(1) + englishWord.slice(0, 1);
+  }
+    console.log(translatedWord);
+    return translatedWord + 'ay';
+  };
+
+var hasVowelAt = function(word, position) {
   var vowels = ['a', 'e', 'i', 'o', 'u']
-  var firstLetter = word.charAt(0);
     for (var i = 0; i < vowels.length; i++) {
-      if (firstLetter === vowels[i]) {
-        return true;
-      }
+    if (word[position] === "y") {
+      if (position > 0) {
+      return true;
     }
-}
+
+    } else if (word[position] === vowels[i]) {
+      return true;
+    }
+  }
+  return false;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,29 @@
-describe('vowelCheck', function() {
-  it("is true for a word that starts with a vowel", function() {
-    expect(vowelCheck('apple')).to.equal(true);
+describe('hasVowelAt', function() {
+  it("returns position of vowel in specified spot", function() {
+    expect(hasVowelAt('snack', 2)).to.equal(true);
+  });
 
+  it('returns false if a word does not have a vowel at specified spot', function (){
+    expect(hasVowelAt('right', 0)).to.equal(false);
+  });
+});
+
+describe('pigLatin', function() {
+  it("adds 'ay' to the end of a word that starts with a", function() {
+    expect(pigLatin("apple")).to.equal("appleay");
+  });
+
+  it("adds 'ay' to the end of any word with a vowel", function() {
+    expect(pigLatin('order')).to.equal('orderay');
+  });  
+});
+
+describe('consonantsCheck', function() {
+  it("is true for a word that starts with a consonant", function() {
+    expect(consonantsCheck('snack')).to.equal(true);
+  });
+
+  it("returns positions of initial consonants", function() {
+    expect(consonantsCheck('told')).to.equal(0);
   });
 });
