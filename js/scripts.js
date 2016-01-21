bc829b91316d862fd52b542493f5047474c2eaa1
Jackson Cafazzo and Will Johnson <trashmoldwilliams@gmail.com>
2016-01-20 16:31:30 -0800

    add refactor, test for qu and y

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,47 @@
 var pigLatin = function(englishWord) {
-  if (hasVowelAt(englishWord, 0)) {
-    var translatedWord = englishWord;
-  } else if (hasVowelAt(englishWord, 3)) {
-    var translatedWord = englishWord.slice(3) + englishWord.slice(0, 3);
-  } else if (hasVowelAt(englishWord, 2)) {
-    var translatedWord = englishWord.slice(2) + englishWord.slice(0, 2);
-  } else if (hasVowelAt(englishWord, 1)) {
-    var translatedWord = englishWord.slice(1) + englishWord.slice(0, 1);
-  }
-    console.log(translatedWord);
-    return translatedWord + 'ay';
+  var splitAt = hasVowelAt(englishWord);
+  var qu = startsWithQU(englishWord);
+  var isY = whereYAt(englishWord);
+
+  if (qu === true) {
+    return englishWord.slice(2) + englishWord.slice(0, 2) + 'ay';
+  } else if (isY > 0) {
+    return englishWord.slice(isY) + englishWord.slice(0, isY) + 'ay';
+  } else
+    return englishWord.slice(splitAt) + englishWord.slice(0, splitAt) + 'ay';
   };
 
-var hasVowelAt = function(word, position) {
-  var vowels = ['a', 'e', 'i', 'o', 'u']
-    for (var i = 0; i < vowels.length; i++) {
-    if (word[position] === "y") {
-      if (position > 0) {
-      return true;
-    }
 
-    } else if (word[position] === vowels[i]) {
-      return true;
+
+var hasVowelAt = function(word) {
+  var vowels = ['a', 'e', 'i', 'o', 'u']
+  var letters = word.split("");
+  for (var i = 0; i < letters.length; i++) {
+    for (var j = 0; j < vowels.length; j++){
+      if (letters[i] === vowels[j]) {
+        return i;
+      }
     }
   }
-  return false;
 };
+
+var startsWithQU = function(word) {
+  if (word.substring(0, 2) === "qu") {
+  return true;
+}
+}
+
+var hasQUin = function(word) {
+  if (word.substring(1, 3) === "qu") {
+    return true;
+  }
+}
+
+var whereYAt = function(word) {
+  var letters = word.split("");
+  for (var i = 0; i < letters.length; i++) {
+    if (letters[i] === 'y') {
+      return i;
+    }
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,10 @@
 describe('hasVowelAt', function() {
   it("returns position of vowel in specified spot", function() {
-    expect(hasVowelAt('snack', 2)).to.equal(true);
+    expect(hasVowelAt('snack', 2)).to.equal(2);
   });
 
   it('returns false if a word does not have a vowel at specified spot', function (){
-    expect(hasVowelAt('right', 0)).to.equal(false);
+    expect(hasVowelAt('right', 1)).to.equal(1);
   });
 });
 
@@ -15,15 +15,24 @@ describe('pigLatin', function() {
 
   it("adds 'ay' to the end of any word with a vowel", function() {
     expect(pigLatin('order')).to.equal('orderay');
-  });  
-});
-
-describe('consonantsCheck', function() {
-  it("is true for a word that starts with a consonant", function() {
-    expect(consonantsCheck('snack')).to.equal(true);
-  });
-
-  it("returns positions of initial consonants", function() {
-    expect(consonantsCheck('told')).to.equal(0);
   });
 });
+
+
+describe("hasQUin", function() {
+  it("returns true if word has 'qu' in (1, 3) position", function() {
+    expect(hasQUin('squitter')).to.equal(true);
+  });
+});
+
+describe("startsWithQU", function () {
+  it("returns true if first two letters are 'qu'", function () {
+    expect(startsWithQU('quitter')).to.equal(true);
+  });
+});
+
+describe("whereYAt", function() {
+  it("find the position of 'y' when deeper than first position", function () {
+    expect(whereYAt('thyme')).to.equal(2);
+  })
+})
